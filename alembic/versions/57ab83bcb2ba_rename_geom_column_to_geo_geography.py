5e25d60fd3f9a5c71c7e38d1299c05d5fe88f956
Andrew Hill <andrew@andrewhill.us>
2016-09-30 03:59:26 +0000

    Rename geom column

---
+++ alembic/versions/57ab83bcb2ba_rename_geom_column_to_geo_geography.py
@@ -0,0 +1,23 @@
+"""Rename geom column to geo (geography)
+
+Revision ID: 57ab83bcb2ba
+Revises: 7d28b515fd64
+Create Date: 2016-09-30 03:48:53.414957
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '57ab83bcb2ba'
+down_revision = '7d28b515fd64'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    op.alter_column('locations', 'geom', new_column_name='geo')
+
+def downgrade():
+    op.alter_column('locations', 'geo', new_column_name='geom')
+

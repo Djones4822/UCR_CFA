0d581911caecdfd89199a3f4083cd4ff736ddaaf
kaushik Visvanathan <kaushik.316@gmail.com>
2016-11-14 20:45:48 -0500

    Updated police one scraper

--- scrape_police_one.py
+++ scrape_police_one.py
@@ -3,9 +3,9 @@ import csv, string, requests
 from bs4 import BeautifulSoup
 from pprint import pprint as pp
 import pandas as pd 
+import time
 #
 #IMPLEMENT DATABASE WRITING
-#
 #
 
 # Dictionary to be overwritten when scraping. Values not found will not change
@@ -27,9 +27,8 @@ class Scraper():
         req = requests.get(search_link)
         if req.status_code != 200:
             return None
-        #print(req.text)
+    
         soup = BeautifulSoup(req.text, 'html.parser')
-
         # Check to see if the last page is the same new page
         head = soup.find('head')
         title = head.find('title').text
@@ -62,13 +61,15 @@ class Scraper():
 
         soup = BeautifulSoup(req.text, 'html.parser')
         dep_name_div = soup.find('h1', {'class':'dep-head-text'})
-
         info = dep_name_div.text
-        try: 
-            name, local = info.split('-',1)
+
+        try: # certain stations have a comma instead of a hyphen in their name
+            name = info.split('-',1)[0] 
             police_dict['Name'] = name 
         except ValueError:
-            print ("Skipped because of not enough or too many values to unpack")
+            print ("No hyphen")
+            name = info.split(',',1)[0]
+            police_dict['Name'] = name 
 
         return_info_dict = police_dict # Make a copy since we only want to overwrite null values that this dept has info for.
         dep_info_div = soup.find_all('div', {'class':'dep-block-info'}) # should return 2 divs
@@ -96,22 +97,23 @@ class Scraper():
             self.search_next = True
             while self.search_next:
                 links = self.get_page_links(state, i)
-                if not links:
+                if links is None:
                     self.agencies[state].append('ERROR: PAGE {}'.format(i))
-                if links == 1:
                     break
-                to_write = []
-                for link in links:
-                    agency_detail = self.get_agency_details(link)
-                    with open('policeone.csv', 'a') as csvfile:
-                        try:
-                            agency_detail.to_csv('policeone.csv', mode='a', header=False)
-                        except AttributeError:
-                            print("Returns a Nonetype Object")
-            
-                i += 1
+                elif links == 1:
+                    break
+                else:
+                    for link in links:
+                        agency_detail = self.get_agency_details(link)
+                        with open('policeone.csv', 'a') as csvfile:
+                            try:
+                                agency_detail.to_csv('policeone.csv', mode='a', header=False) # Appends a dataframe to csv ; each df is one police station
+                            except AttributeError:
+                                print("Returns a Nonetype Object")
+
+                    i += 1
 
 
 scraper = Scraper()
 scraper.run()
-csvfile.close()
+
